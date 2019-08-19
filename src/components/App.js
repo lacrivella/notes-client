fd0061f36420109440127a5de837f5788d8c57d8
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-19 10:43:04 -0700

    added NoteForm to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
+import NoteForm from './NoteForm';
 export default function App() {
-  return <h1>Hello World</h1>;
-}
+  return <NoteForm title="hi" body="yo yo" />;
+};
   
